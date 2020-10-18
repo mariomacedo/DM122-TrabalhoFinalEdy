60c1dfe0229cb7eb440ce5de054ca8d65e2bf7ac
Mario Macedo <mguilhermemacedo@gmail.com>
2020-10-18 20:03:15 -0300

    style: PWA logo

--- sw.js
+++ sw.js
@@ -1,5 +1,18 @@
 const cacheName = "app-shell-v2";
-const assetsToCache = ["offline.html"];
+const assetsToCache = [
+  "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+  "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+  "https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2",
+  "https://fonts.googleapis.com/css?family=Roboto:400,700",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "assets/images/pwa-logo.png",
+  "assets/js/material.min.js",
+  "assets/css/style.css",
+  "assets/js/app.js",
+  "favicon.ico",
+  "index.html",
+  "/",
+];
 
 function removeOldCache(key) {
   if (key !== cacheName) {
